ea21df324bc3a8a2236245977fbcc77d03f56f3e
Terminator0992 <72911724+Terminator0992@users.noreply.github.com>
2022-10-11 20:12:07 +0530

    Update new1.c

--- C++ programs/new1.c
+++ C++ programs/new1.c
@@ -5,6 +5,5 @@ int main()
     float a,b;
     printf("Enter the distance in inches: ");
     scanf("%f",&a);
-    b= a*2.54;
-    printf("Distance %f inches is =%f cms",a,b);
+    ___your code____
 } 
