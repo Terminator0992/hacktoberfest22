5d5b7d47b292a0ce529b4f8b10ec645f21a83c16
Terminator0992 <72911724+Terminator0992@users.noreply.github.com>
2022-10-16 21:33:09 +0530

    Update displaywords.py

--- Python programs/displaywords.py
+++ Python programs/displaywords.py
@@ -3,8 +3,4 @@ def DISPLAYWORDS():
     file=open('xyz.txt','r')
     line=file.read()
     word=line.split()
-    for w in word:
-        if len(w)<4:
-            print(w)
-    file.close()
-DISPLAYWORDS()
+    ==your code==
